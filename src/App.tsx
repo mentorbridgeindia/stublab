13376756e9994e0bf99d5f0ee7d0ce0deb019fdc
vijayalakshmithirumoorthy810@gmail.com <vijayalakshmiTHIRUMOORTHY>
2024-12-05 15:39:18 +0530

    file created

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
