12d9b8f438cd5deb7cc4fedb05a177e7de1269fa
Umarfarook <ujuj2451@gmail.com>
2024-12-05 15:55:20 +0530

    Api ui

---
+++ src/test/java/org/mentorbridge/stublab/StublabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mentorbridge.stublab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StublabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
