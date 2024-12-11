d290d9e539a9475dd9188d22393542766de2e356
Umarfarook <ujuj2451@gmail.com>
2024-12-11 10:04:44 +0530

    ui design completed for desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ModelForm from './pages/model-form/modelform';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Routes>
+        <Route path="/" element={<ModelForm />} />
+        
+      </Routes>
     </div>
+  </Router>
   );
 }
 
