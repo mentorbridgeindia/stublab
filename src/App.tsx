6f27bad0f04712f18c61293c920b4b0cde49d29c
Mentor Bridge <mentorbridgeindia@gmail.com>
2024-12-29 08:32:12 +0000

    removed unwanted changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ import Container from "react-bootstrap/esm/Container";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.scss";
-import CustomApi from "./page/CustomApi"; 
-import "bootstrap/dist/css/bootstrap.min.css";
-import OrgPage from "./page/CustomApi";
 
 
 function App() {
@@ -18,8 +15,6 @@ function App() {
     <div className="App">
       <ToastContainer position="bottom-left" />
       <Header />
-    
-      <CustomApi/>
       <Container className="well-container">
         <BrowserRouter>
           <Routes>
@@ -27,8 +22,6 @@ function App() {
             <Route path="/application" element={<ApplicationIndexPage />} />
             <Route path="/model" element={<ModelIndexPage />} />
             <Route path="/model/create" element={<CreateModelPage />} />
-          
-
           </Routes>
         </BrowserRouter>
       </Container>
