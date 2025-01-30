c995a56cab4e9b9e85f616970c7e14a812e4b25f
Vijayalakshmi1088 <vijayalakshmifsd@gmail.com>
2025-01-30 01:55:16 -0800

    Api Call using Tanstack

--- src/pages/Organization/CreatePage/CreateOrganizationPage.tsx
+++ src/pages/Organization/CreatePage/CreateOrganizationPage.tsx
@@ -4,7 +4,7 @@ import CreateOrganization from "@/modules/Organization/CreateOrganization/Create
 export const CreateOrganizationPage = () => {
   return (
     <>
-      <h1 className="my-4 text-center">CreateOrganization</h1>
+      <h1 className="my-4 text-center">createOrganization</h1>
       <CreateOrganization/>
     </>
   );
