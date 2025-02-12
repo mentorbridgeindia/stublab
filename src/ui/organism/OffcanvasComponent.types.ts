7aee380037d6d468e92cb3921812dd5a1282d363
Karthikeyan <137636670+karthi049@users.noreply.github.com>
2025-02-12 12:39:13 +0530

    Merged main into this branch

--- src/ui/organism/OffcanvasComponent.types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface OffcanvasComponentProps {
-  buttonLabel: any;        
-  title: string;                 
-  children: ReactNode;            
-  placement: 'start' | 'end' | 'top' | 'bottom'; 
-  buttonVariant?: string;      
-}
