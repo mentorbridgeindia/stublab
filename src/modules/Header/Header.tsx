9652411a7467fbf72a69da0110a53995ff96e2af
Umarfarook <ujuj2451@gmail.com>
2025-02-15 14:51:54 +0530

    Account page redirect with token

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -5,6 +5,10 @@ import Navbar from "react-bootstrap/Navbar";
 import "./Header.css";
 
 export const Header = () => {
+  const jwtToken = sessionStorage.getItem("accessToken");
+
+  const accountURL=`https://stublab.securosphere.in/profile?token=${jwtToken}`;
+ 
   return (
     <Navbar fixed="top" className="bg-primary">
       <Container>
@@ -14,7 +18,7 @@ export const Header = () => {
         <Nav className="ms-auto">
           <Nav.Link href="/application">Applications</Nav.Link>
           <Nav.Link href="/model">Models</Nav.Link>
-          <Nav.Link href="/account">Account</Nav.Link>
+          <Nav.Link href={accountURL} target="_blank" rel="noopener noreferrer">Account</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
