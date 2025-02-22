8565b7ad761b27aaa0d7e49d4bc27b6b04b0d8a4
Umarfarook <ujuj2451@gmail.com>
2025-02-22 16:51:29 +0530

    url validation

--- src/modules/CustomAPI/schema.ts
+++ src/modules/CustomAPI/schema.ts
@@ -2,7 +2,10 @@ import * as yup from "yup";
 import { dropdownData } from "./dropdownData";
 
 export const schema = yup.object().shape({
-  url: yup.string().url("Please enter a valid URL").required("URL is required"),
+  url: yup.string().matches(
+    /^\/[a-zA-Z]+(\/{[a-zA-Z0-9_-]+\})?$/,
+    "Invalid website URL"
+  ).required("URL is required"),
   name: yup.string().required("Name is required"),
   method: yup
     .string()
