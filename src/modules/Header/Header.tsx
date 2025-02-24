fe44d51ec38e39ba732cba1742a22c288fed7393
Karthikeyan <137636670+karthi049@users.noreply.github.com>
2025-02-24 22:17:56 +0530

    Navbar enhanced

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -3,20 +3,24 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Header.css";
-import { color } from "@uiw/react-codemirror";
 
 export const Header = () => {
   return (
-    <Navbar fixed="top">
+    <Navbar fixed="top" expand="lg">
       <Container>
         <Navbar.Brand href="/" className="logo-lg">
           <IconLogo />
         </Navbar.Brand>
-        <Nav className="ms-auto">
-          <Nav.Link href="/application">Applications</Nav.Link>
-          <Nav.Link href="/model">Models</Nav.Link>
-          <Nav.Link href="/account">Account</Nav.Link>
-        </Nav>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/application">Applications</Nav.Link>
+            <Nav.Link href="/model">Models</Nav.Link>
+            <Nav.Link href="/account">Account</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
