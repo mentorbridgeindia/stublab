a4ee01ca990bb47541743225a45dfb8e3eaf48b1
Vijaykarthik1 <142250291+Vijaykarthik1@users.noreply.github.com>
2025-02-28 11:22:08 +0530

    Delete integration (#42)
    * Delete Integerated
    
    * DeleteModel Style Changed
    
    * Implemented queryClient to avoid refresh
    
    * resolve conflicts

--- src/modules/Model/CreateModel/hooks/useCreateModelForm.ts
+++ src/modules/Model/CreateModel/hooks/useCreateModelForm.ts
@@ -2,6 +2,7 @@ import { useFieldArray } from "react-hook-form";
 
 import { IModelMutation } from "@entities/Model";
 import { IUseCreateModel } from "../CreateModel.types";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const useCreateModelForm = ({ form, onFormSubmit }: IUseCreateModel) => {
   const {
@@ -16,9 +17,11 @@ export const useCreateModelForm = ({ form, onFormSubmit }: IUseCreateModel) => {
     control,
     name: "variables",
   });
+  const queryClient = useQueryClient();
 
   const onSubmit = async (data: IModelMutation) => {
     onFormSubmit(data, reset);
+    queryClient.invalidateQueries({queryKey:["createmodel"]})
   };
 
   return {
